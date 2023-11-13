cab03bc47cb16094ced7126f6c791d00e54806f3
Nelson Estevão <d12733@di.uminho.pt>
2023-11-13 09:18:47 +0000

    🌅

---
+++ lib/Tarefa1.hs
@@ -0,0 +1,17 @@
+{-|
+Module      : Tarefa1
+Description : Verifica colisões
+Copyright   : Nuno Miguel Paiva Fernandes <a107317@alunos.uminho.pt>
+              Pedro Herculano Soares Oliveira do Lago Esteves <a106839@alunos.uminho.pt>
+
+Módulo para a realização da Tarefa 1 de LI1 em 2023/24.
+-}
+module Tarefa1 where
+
+import LI12324
+
+colisoesParede :: Mapa -> Personagem -> Bool
+colisoesParede = undefined
+
+colisoesPersonagens :: Personagem -> Personagem -> Bool
+colisoesPersonagens = undefined
---
+++ lib/Tarefa2.hs
@@ -0,0 +1,14 @@
+{-|
+Module      : Tarefa2
+Description : Valida jogo
+Copyright   : Nuno Miguel Paiva Fernandes <a107317@alunos.uminho.pt>
+              Pedro Herculano Soares Oliveira do Lago Esteves <a106839@alunos.uminho.pt>
+
+Módulo para a realização da Tarefa 2 de LI1 em 2023/24.
+-}
+module Tarefa2 where
+
+import LI12324
+
+valida :: Jogo -> Bool
+valida = undefined
