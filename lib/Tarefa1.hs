2bd689edf921587090cf4587e738f8efd1849f6d
Nuno Fernandes <nmpf.2005@gmail.com>
2023-11-16 19:43:30 +0000

    added test map

--- lib/Tarefa1.hs
+++ lib/Tarefa1.hs
@@ -10,8 +10,20 @@ module Tarefa1 where
 
 import LI12324
 
+mapaTeste = Mapa ((0.5, 5.5), Oeste) (0.5, 2.5)
+    [[Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma]
+    ,[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio]
+    ,[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio]
+    ,[Plataforma, Plataforma, Vazio, Vazio, Vazio, Vazio, Plataforma, Plataforma, Plataforma, Plataforma]
+    ,[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Escada, Vazio]
+    ,[Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Vazio, Escada, Vazio]
+    ,[Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma, Plataforma]
+    ]
+
 colisoesParede :: Mapa -> Personagem -> Bool
 colisoesParede = undefined
+
+
 
 colisoesPersonagens :: Personagem -> Personagem -> Bool
 colisoesPersonagens = undefined
