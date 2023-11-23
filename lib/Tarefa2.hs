2bcdeb87edf46a1347289cace8963f7b460b71a2
Herculano Esteves <pedroesteves005@gmail.com>
2023-11-23 15:58:37 +0000

    Tarefa2 mais

--- lib/Tarefa2.hs
+++ lib/Tarefa2.hs
@@ -13,7 +13,7 @@ import Tarefa1 (sobreposicao, genHitbox)
 
 -- Test data START
 enmLs :: [Personagem]
-enmLs = [Personagem {ressalta = True, posicao = (20,1.5), tamanho = (7,7)},Personagem {ressalta = True, posicao = (20,1.5), tamanho = (7,7)},Personagem {ressalta = True, posicao = (20,7.5), tamanho = (7,7)}]
+enmLs = [Personagem {ressalta = True, posicao = (1.5,1.5), tamanho = (7,7)},Personagem {ressalta = True, posicao = (20,1.5), tamanho = (7,7)},Personagem {ressalta = True, posicao = (20,7.5), tamanho = (7,7)}]
 jog :: Personagem
 jog = Personagem {ressalta = False, posicao = (1.5,2.5), tamanho = (7,7)}
 -- Test data END
@@ -23,6 +23,7 @@ jog = Personagem {ressalta = False, posicao = (1.5,2.5), tamanho = (7,7)}
 valida :: Jogo -> Bool
 valida = undefined
 
+-- | Verifica o chao do mapa
 validaChao :: Mapa -> Bool
 validaChao (Mapa _ _ mapMat) = all (== Plataforma) (last mapMat)
 
@@ -30,5 +31,17 @@ validaChao (Mapa _ _ mapMat) = all (== Plataforma) (last mapMat)
 validaRessalta :: Personagem -> [Personagem] -> Bool
 validaRessalta jogador inimigosList = not (ressalta jogador) && all ressalta inimigosList
 
+-- | Verifica a posiçao inicial se sobrepoem ou nao com os inimigos
 validaPosJogInim :: Personagem -> [Personagem] -> Bool
 validaPosJogInim jogador inimigosList = all (==False) (map (\i -> sobreposicao (genHitbox i) (genHitbox jogador)) inimigosList)
+
+-- | Verifica o numero minimo de inimigos
+validaInimigmin :: [Personagem] -> Bool
+validaInimigmin x = length x >= 2
+
+-- | Verifica a vida de fantasmas
+validaVidaghost :: [Personagem] -> Bool
+validaVidaghost [] = True
+validaVidaghost (h:t)   | tipo h == Fantasma && vida h == 1 = True
+                        | otherwise = validaInimigmin t
+
