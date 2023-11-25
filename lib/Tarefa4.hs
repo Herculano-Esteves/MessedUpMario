d8ad98643b6d0eb429bc33a2bf8b63e1318dfbb8
Nuno Fernandes <nmpf.2005@gmail.com>
2023-11-25 19:09:27 +0000

    Started tarefa 3

--- lib/Tarefa4.hs
+++ lib/Tarefa4.hs
@@ -14,3 +14,17 @@ import LI12324
 
 atualiza :: [Maybe Acao] -> Maybe Acao -> Jogo -> Jogo
 atualiza = undefined
+
+atualizaInimigos :: [Maybe Acao] -> [Personagem] -> [Personagem]
+atualizaInimigos actions inms = zipWith (\a b -> b) actions inms
+
+-- * Change the velocity
+-- TODO: Define how each character is going to jump
+atualizaInimigo :: Maybe Acao -> Personagem -> Personagem
+atualizaInimigo action inm = case action of
+        Just Subir -> inm {velocidade = (0,-10)}
+        Just Descer -> inm {velocidade = (0,10)}
+        Just AndarEsquerda -> inm {velocidade = (-10,0), direcao = Oeste}
+        Just AndarDireita -> inm {velocidade = (10,0), direcao = Este}
+        Just Parar -> inm {velocidade = (0,0)}
+        Nothing -> inm
